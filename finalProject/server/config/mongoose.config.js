04a7296c556f82d0239b65a9e15a18fdb9b8a11c
Bushra AlQahtani <89680878+bushra-alqahtani@users.noreply.github.com>
2022-09-08 13:26:52 +0300

    Delete finalProject directory

--- finalProject/server/config/mongoose.config.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require("mongoose");
-
-mongoose
-  .connect("mongodb://localhost/"+process.env.DATABASE_NAME)
-  .then(() => console.log("Succesfully connected to the database"))
-  .catch((err) => console.log("Failed to connect to the database"));
